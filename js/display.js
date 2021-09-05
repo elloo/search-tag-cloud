b71c52baddd69e5cdb873a6512276f0f72979a7b
Ewe Lin Loo <ewelinloo@gmail.com>
2021-09-05 19:21:52 +1000

    Upload for submission

---
+++ js/display.js
@@ -0,0 +1,40 @@
+const searchForm = document.getElementById('fetchImagesWrapper')
+
+searchForm.addEventListener('submit', displayNewImage)
+
+async function displayNewImage(e, tagSearchTerm){
+  e.preventDefault()
+  const imageToDisplay = document.getElementById('imageToDisplay')
+  const attribution = document.getElementById('attribution')
+  const submittedTerm = document.getElementById('searchTerm').value
+  const searchTerm = tagSearchTerm ? tagSearchTerm : submittedTerm
+  if (searchTerm.match(/[^a-z]/gi)){
+    alert('Alphabetical letters and ONE word only!')
+  } else {
+    const requestUrl = `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=R2vv_O0n02TBFMvHRKuDW0l_mubcSqoIy3_qokCiWr0`
+    const randomImage = await getNewImage(requestUrl)
+    if (randomImage){
+      addTag(searchTerm.toLowerCase())
+      const photographerUrl = `${randomImage.user.links.html}?utm_source=Photo_tag_cloud&utm_medium=referral`
+      const unsplashUrl = 'https://unsplash.com/?utm_source=Photo_tag_cloud&utm_medium=referral'
+      const attributionString = `<i>Photo by <a href="${photographerUrl}">${randomImage.user.name}</a> on <a href="${unsplashUrl}">Unsplash</a></i>`
+      attribution.innerHTML = attributionString
+      imageToDisplay.src = randomImage.urls.regular
+    } else {
+      const placeholder = 'https://via.placeholder.com/550x350.png?text=Nothing+to+see+here...'
+      imageToDisplay.src =  placeholder
+      attribution.innerHTML = ''
+    }
+  }
+}
+
+async function getNewImage(requestUrl) {
+const randomNumber = Math.floor(Math.random() * 10)
+return fetch(requestUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    const image = data.results[randomNumber]
+    return image
+  })
+  .catch((error) => console.error('Fetch error: ', error));
+}
---
+++ js/tagCloud.js
@@ -0,0 +1,40 @@
+let searchArray = []
+const fragment = document.createDocumentFragment()
+
+function addTag(searchTerm){
+    const termIndex = searchArray.findIndex(object => object.term === searchTerm)
+    const tagsList = document.getElementById('tags')
+    // If term exists in searchArray
+    if (termIndex !== -1){
+        searchArray[termIndex].searchCount++
+    } else {    
+        termObject = {term: searchTerm, searchCount: 1}
+        searchArray.push(termObject)
+    }
+    orderedSearchArray = [...searchArray]
+    orderedSearchArray.sort((a,b) => parseInt(b.searchCount) - parseInt(a.searchCount))
+    const highestSearchCount = orderedSearchArray[0].searchCount
+    searchArray.forEach(searchTerm => handleTerm(searchTerm, highestSearchCount))
+    tagsList.innerHTML = ''
+    tagsList.appendChild(fragment)
+}
+
+function handleTerm(termObject, highestSearchCount){
+    const numberOfTerms = termObject.searchCount
+    const term = termObject.term
+    const maxTagSize = 6
+    const tag = document.createElement('li')
+    
+    let fontSize = numberOfTerms / highestSearchCount * maxTagSize
+    fontSize = fontSize.toFixed()
+    if (fontSize <= 1) fontSize = '1'
+    const fontSizeProperty = fontSize
+
+    if (fontSizeProperty === '1' || fontSizeProperty === '2') tag.classList.add('least')
+    if (fontSizeProperty === '3' || fontSizeProperty === '4') tag.classList.add('average')
+    if (fontSizeProperty === '5' || fontSizeProperty === '6') tag.classList.add('most')
+    
+    tag.classList.add('tag')
+    tag.innerHTML = `<span onclick="displayNewImage(event, '${term}')" class="tagLink" style="font-size: ${fontSizeProperty}em">${term} (${termObject.searchCount}) </span>`
+    fragment.appendChild(tag)
+}
